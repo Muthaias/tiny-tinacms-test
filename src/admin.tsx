ca0fb4464c2ca3caeaf942841db8b146358dc948
Mattias Nyberg <muthaias.uu@gmail.com>
2020-05-18 17:10:09 +0200

    feat: add optional data seeding of CMS for demo purposes

--- src/admin.tsx
+++ src/admin.tsx
@@ -16,18 +16,48 @@ import {
 } from "react-router-dom";
 import {CMSProvider} from "./components/cms";
 
-ReactDOM.render(
-    <Router>
-        <CMSProvider
-            init={() => cmsFromStores(
-                GenericEntryStore.fromTargetId<Post>("__posts"),
-                GenericEntryStore.fromTargetId<Post>("__pages"),
-                GenericEntryStore.fromTargetId<Menu>("__menu"),
-                GenericEntryStore.fromTargetId<Author>("__authors")
-            )}
-        >
-            <Application />
-        </CMSProvider>
-    </Router>,
-    document.getElementById("app")
-);
+function renderSite(
+    targetId: string = "app"
+) {
+    ReactDOM.render(
+        <Router>
+            <CMSProvider
+                init={() => cmsFromStores(
+                    GenericEntryStore.fromTargetId<Post>("posts"),
+                    GenericEntryStore.fromTargetId<Post>("pages"),
+                    GenericEntryStore.fromTargetId<Menu>("menus"),
+                    GenericEntryStore.fromTargetId<Author>("authors")
+                )}
+            >
+                <Application />
+            </CMSProvider>
+        </Router>,
+        document.getElementById(targetId)
+    );
+}
+
+const shouldSeed: boolean = true;
+const forceSeed: boolean = false;
+(async () => {
+    const seedIds = [
+        "posts",
+        "pages",
+        "authors",
+        "menus",
+    ]
+    const needSeed: boolean = !seedIds.reduce((acc, id) => acc || !!window.localStorage.getItem(id), false);
+    if (shouldSeed && needSeed || forceSeed) {
+        try {
+            const response = await fetch("./data/data.json");
+            const data = await response.json();
+            seedIds.forEach((id) => {
+                window.localStorage.setItem(id, JSON.stringify(data[id] || []))
+            });
+        } catch (e) {
+            console.warn("Unable to seed local storage data.");
+        }
+    }
+    
+    renderSite("app");
+})();
+
